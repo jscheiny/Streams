c3aa056ff5bb865d223dcac5476b0af62d651253
Jonah Scheinerman <jscheinerman@palantir.com>
2014-05-17 16:22:37 -0700

    Refactor set intersection as set operation

--- SetIntersectionStreamProvider.h
+++ SetIntersectionStreamProvider.h
@@ -5,66 +5,41 @@
 #include "Utility.h"
 
 template<typename T, typename Compare>
-class SetIntersectionStreamProvider : public StreamProvider<T> {
+class SetIntersectionStreamProvider
+            : public SetOperationStreamProvider<T, Compare> {
+
+    using Parent = SetOperationStreamProvider<T, Compare>;
+    using UpdateState = typename Parent::UpdateState;
+    using ToAdvance = typename Parent::ToAdvance;
 
 public:
     SetIntersectionStreamProvider(StreamProviderPtr<T> source1,
                                   StreamProviderPtr<T> source2,
                                   Compare&& comparator)
-        : source1_(std::move(source1)),
-          source2_(std::move(source2)),
-          comparator_(comparator) {}
+          : Parent(std::forward<StreamProviderPtr<T>>(source1),
+                   std::forward<StreamProviderPtr<T>>(source2),
+                   std::forward<Compare>(comparator)) {}
 
-    std::shared_ptr<T> get() override {
-        return result_;
-    }
-
-    bool advance() override {
-        if(depletion_ == DepleteState::Either) {
-            return false;
-        }
-
-        advance_stream(source1_, current1_);
-        advance_stream(source2_, current2_);
-
-        while(depletion_ == DepleteState::Neither) {
-            if(comparator_(*current1_, *current2_)) { // curr1 < curr2
-                advance_stream(source1_, current1_);
-            } else if(comparator_(*current2_, *current1_)) { // curr2 < curr1
-                advance_stream(source2_, current2_);
-            } else { // curr1 == curr2
-                result_ = current1_;
-                return true;
-            }
-        }
-        return false;
-    }
-
-private:
-    enum class DepleteState {
-        Neither,
-        Either
-    };
-
-    DepleteState depletion_ = DepleteState::Neither;
-
-    Compare comparator_;
-    StreamProviderPtr<T> source1_;
-    StreamProviderPtr<T> source2_;
-    std::shared_ptr<T> current1_;
-    std::shared_ptr<T> current2_;
-    std::shared_ptr<T> result_;
-
-    bool advance_stream(StreamProviderPtr<T>& source,
-                        std::shared_ptr<T>& current) {
-        if(source->advance()) {
-            current = source->get();
-            return true;
+protected:
+    UpdateState if_neither_depleted() override {
+        if(this->current1_smaller()) {
+            this->set_advance(ToAdvance::First);
+        } else if(this->current2_smaller()) {
+            this->set_advance(ToAdvance::Second);
         } else {
-            current.reset();
-            depletion_ = DepleteState::Either;
-            return false;
+            this->set_advance(ToAdvance::Both);
+            this->set_result(this->get_current1());
+            return UpdateState::UpdateFinished;
         }
+        return UpdateState::NotFinished;
+    }
+
+    virtual UpdateState if_first_depleted() {
+        return UpdateState::StreamFinished;
+    }
+
+    virtual UpdateState if_second_depleted() {
+        return UpdateState::StreamFinished;
     }
 
 };
