3d364da258d47ab0ceb850f8e12792c983c69c46
Jonah Scheinerman <jonah@scheinerman.net>
2014-08-21 12:21:41 -0400

    Add tests for MakeStream::empty

---
+++ test/EmptyTest.cpp
@@ -0,0 +1,17 @@
+#include <Stream.h>
+
+#include <gmock/gmock.h>
+
+using namespace testing;
+using namespace stream;
+using namespace stream::op;
+
+TEST(EmptyTest, MakeStream) {
+    EXPECT_THAT(
+        MakeStream::empty<int>() | to_vector(),
+        ElementsAre());
+}
+
+TEST(EmptyTest, Constructor) {
+    EXPECT_THAT(Stream<int>() | to_vector(), ElementsAre());
+}
