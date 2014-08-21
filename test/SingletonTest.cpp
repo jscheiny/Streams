ff443b92bfcaf61eca9d2632e3dc380a89ea3563
Jonah Scheinerman <jonah@scheinerman.net>
2014-08-21 12:40:34 -0400

    Add tests for MakeStream::singleton

---
+++ test/SingletonTest.cpp
@@ -0,0 +1,11 @@
+#include <Stream.h>
+
+#include <gmock/gmock.h>
+
+using namespace testing;
+using namespace stream;
+using namespace stream::op;
+
+TEST(Singleton, Default) {
+    EXPECT_THAT(MakeStream::singleton(5) | to_vector(), ElementsAre(5));
+}
